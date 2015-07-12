6a0ae49c678af0e70da00da54f08483cfc20f994
Artur Salyakhov <artykod@gmail.com>
2015-07-12 22:18:26 +0500

    - рендеринг переключен на deferred shading для оптимизации draw calls (600 вместо 1500) - бордюр на краю пристани - доп анимация моря - правка материалов объектов парковки - правки в уи - чистка мусора в проекте

---
+++ Assets/scripts/PositionAnimation.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PositionAnimation : MonoBehaviour {
+	[SerializeField]
+	private Vector3 offset = Vector3.zero;
+
+	private Vector3 position = Vector3.zero;
+	private float angle = 0f;
+
+	private void Awake() {
+		position = transform.localPosition;
+	}
+
+	private void Update () {
+		Vector3 rotated = (Quaternion.Euler(0f, 0f, angle) * offset);
+		Vector3 pos = position;
+		pos.x += rotated.x;
+		pos.z += rotated.y;
+        transform.localPosition = pos;
+
+		angle += Time.deltaTime * 20f;
+    }
+}
